50c9e1f0ae1846eb465e10c2bf38d2928caa4488
miaoli-psy <chuoli223@yahoo.com>
2021-12-03 19:45:42 +0100

    6 participants

--- src/rm_face_exp1.py
+++ src/rm_face_exp1.py
@@ -4,32 +4,35 @@ from src.common.process_basic_data_sturcture import cal_SEM
 from src.common.process_dataframe import insert_new_col_from_two_cols, rename_df_col
 
 if __name__ == '__main__':
+    is_main_exp = True
     PATH = "../exp1/data/"
-    DATA = "rm_face_exp1_preprocessed.xlsx"
-
+    if is_main_exp:
+        DATA = "rm_face_exp1_preprocessed.xlsx"
+    else:
+        DATA = "rm_face_exp1_discri_preprocessed.xlsx"
     to_excel = False
 
     # read data
     data = pd.read_excel(PATH + DATA)
 
     # averaged data: averaged deviation for each condition per participant (separate spacing)
-    data_1 = data.groupby(["spacing_in_deg", "setsize", "stimulus_size", "participant", "stimulus_type"])[
+    data_1 = data.groupby(["spacing_in_deg", "setsize", "stimulus_size", "participant", "type"])[
         "deviation_score"] \
         .agg(['mean', 'std']) \
-        .reset_index(level = ["spacing_in_deg", "setsize", "stimulus_size", "participant", "stimulus_type"])
+        .reset_index(level = ["spacing_in_deg", "setsize", "stimulus_size", "participant", "type"])
 
     rename_df_col(df = data_1, old_col_name = "mean", new_col_name = "mean_deviation_score")
     data_1["samplesize"] = [4] * data_1.shape[0]  # each participant repeat each condition 4 times
     insert_new_col_from_two_cols(data_1, "mean_deviation_score", "samplesize", "SEM", cal_SEM)
 
     # averaged data: averaged deviation for each condition across participant (separate spacing)
-    data_2 = data.groupby(["spacing_in_deg", "setsize", "stimulus_size", "stimulus_type"])[
+    data_2 = data.groupby(["spacing_in_deg", "setsize", "stimulus_size", "type"])[
         "deviation_score"] \
         .agg(['mean', 'std']) \
-        .reset_index(level = ["spacing_in_deg", "setsize", "stimulus_size", "stimulus_type"])
+        .reset_index(level = ["spacing_in_deg", "setsize", "stimulus_size", "type"])
 
     rename_df_col(df = data_2, old_col_name = "mean", new_col_name = "mean_deviation_score")
-    data_2["samplesize"] = [20] * data_2.shape[0]  # each condition repeat 4*5 times
+    data_2["samplesize"] = [24] * data_2.shape[0]  # each condition repeat 4*6 times
     insert_new_col_from_two_cols(data_2, "mean_deviation_score", "samplesize", "SEM", cal_SEM)
 
     if to_excel:

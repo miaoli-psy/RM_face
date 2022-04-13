fab4bed507e2b4c45677df45c35bacaeb3dca32a
miaoli-psy <chuoli223@yahoo.com>
2022-04-13 17:23:38 +0200

    cal d_prime, c, beta etc from SDT

---
+++ src/preprocess/rm_face_exp2_cal_CDT.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from scipy.stats import norm
+import math
+
+Z = norm.ppf
+
+totalData = pd.read_csv("../../data/dprime.csv")
+
+
+def SDT(hits, misses, fas, crs):
+    """ returns a dict with d-prime measures given hits, misses, false alarms, and correct rejections"""
+    # Floors an ceilings are replaced by half hits and half FA's
+    half_hit = 0.5 / (hits + misses)
+    half_fa = 0.5 / (fas + crs)
+
+    # Calculate hit_rate and avoid d' infinity
+    hit_rate = hits / (hits + misses)
+    if hit_rate == 1:
+        hit_rate = 1 - half_hit
+    if hit_rate == 0:
+        hit_rate = half_hit
+
+    # Calculate false alarm rate and avoid d' infinity
+    fa_rate = fas / (fas + crs)
+    if fa_rate == 1:
+        fa_rate = 1 - half_fa
+    if fa_rate == 0:
+        fa_rate = half_fa
+
+        # Calculate hit_rate and avoid d' infinity
+    hit_rate = hits / (hits + misses)
+    if hit_rate == 1:
+        hit_rate = 1 - half_hit
+    if hit_rate == 0:
+        hit_rate = half_hit
+
+    # Calculate false alarm rate and avoid d' infinity
+    fa_rate = fas / (fas + crs)
+    if fa_rate == 1:
+        fa_rate = 1 - half_fa
+    if fa_rate == 0:
+        fa_rate = half_fa
+    out = {}
+    out['d'] = Z(hit_rate) - Z(fa_rate)
+    out['beta'] = math.exp((Z(fa_rate) ** 2 - Z(hit_rate) ** 2) / 2)
+    out['c'] = -(Z(hit_rate) + Z(fa_rate)) / 2
+    out['Ad'] = norm.cdf(out['d'] / math.sqrt(2))
+
+    return (out)
+
+
+totalData["SDT"] = totalData.apply(lambda x: SDT(x["hit"], x["miss"], x["FA"], x["CR"]), axis = 1)

05d6eaa0f26b008a84add4c37998b065c60ce03d
miaoli-psy <chuoli223@yahoo.com>
2022-11-18 09:56:00 +0100

    add exp1 wide data

--- src/preprocess/rm_face_exp1_widedata.py
+++ src/preprocess/rm_face_exp1_widedata.py
@@ -1,8 +1,14 @@
-# -*- coding: utf-8 -*- 
-"""
-Project: RM_face
-Creator: Miao
-Create time: 2022-10-19 14:25
-IDE: PyCharm
-Introduction:
-"""
+import pandas as pd
+
+if __name__ == '__main__':
+    PATH = "../../data/"
+    FILE = "data_by_subject_long.csv"
+
+    data_long = pd.read_csv(PATH + FILE, index_col=[0])
+
+    table = pd.pivot_table(data_long,
+                           values = ["deviation_score_mean"],
+                           index = ["participant"],
+                           columns = ["setsize", "stimulus_types", "size_scale"])
+
+    table.to_excel("data_wide.xlsx")
